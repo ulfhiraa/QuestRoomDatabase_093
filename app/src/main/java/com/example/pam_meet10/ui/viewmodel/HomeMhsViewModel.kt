8b2bc88d87cefbd5a157d88f191c54d56f39359c
ulfhiraa <studyiraa@gmail.com>
2024-12-13 22:36:26 +0700

    UPDATE kode HomeMhsViewModel

--- app/src/main/java/com/example/pam_meet10/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/pam_meet10/ui/viewmodel/HomeMhsViewModel.kt
@@ -7,7 +7,11 @@ import com.example.pam_meet10.repository.RepositoryMhs
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.internal.NopCollector.emit
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class HomeMhsViewModel (
     private val repositoryMhs: RepositoryMhs
