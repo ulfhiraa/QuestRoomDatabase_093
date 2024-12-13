b1f3a672b7915e7b65cfce79399293f967e7fd20
ulfhiraa <studyiraa@gmail.com>
2024-12-13 22:35:30 +0700

    ADD implementasi member untuk menghubungkan MahasiswaDao dan operasi yang didefinisikan dalam RepositoryMhs

--- app/src/main/java/com/example/pam_meet10/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/pam_meet10/repository/LocalRepositoryMhs.kt
@@ -2,11 +2,28 @@ package com.example.pam_meet10.repository
 
 import com.example.pam_meet10.data.dao.MahasiswaDao
 import com.example.pam_meet10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs (
     private val mahasiswaDao: MahasiswaDao
 ) : RepositoryMhs { // implementasi dari RepositoryMhs
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deteleMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
     }
 }
