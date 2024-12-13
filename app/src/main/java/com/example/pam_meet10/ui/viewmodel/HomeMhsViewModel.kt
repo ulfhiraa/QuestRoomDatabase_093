00a3fae3b9569b69e0a500539d91925a96496361
ulfhiraa <studyiraa@gmail.com>
2024-12-13 19:39:19 +0700

    ADD Data Class HomeUiState pada HomeMhsViewModel

--- app/src/main/java/com/example/pam_meet10/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/pam_meet10/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.pam_meet10.ui.viewmodel
 
-class HomeMhsViewModel {
+import androidx.core.app.NotificationCompat.MessagingStyle.Message
+import androidx.lifecycle.ViewModel
+import com.example.pam_meet10.data.entity.Mahasiswa
+import com.example.pam_meet10.repository.RepositoryMhs
+import kotlinx.coroutines.flow.StateFlow
+
+class HomeMhsViewModel (
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel(){
+
 }
+
+data class HomeUiState(
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoading: Boolean = false,
+    val Error: Boolean = false,
+    val errorMessage: String = ""
+)
