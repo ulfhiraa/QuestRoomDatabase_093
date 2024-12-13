5fbf8c1695f8f3174d19f3ddebcf312a9e5e6049
ulfhiraa <studyiraa@gmail.com>
2024-12-13 22:28:17 +0700

    ADD fungsi untuk mengelola data mahasiswa

--- app/src/main/java/com/example/pam_meet10/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pam_meet10/repository/RepositoryMhs.kt
@@ -1,7 +1,16 @@
 package com.example.pam_meet10.repository
 
 import com.example.pam_meet10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
-    suspend fun insertMhs(mahasiswa: Mahasiswa)
+    suspend fun insertMhs(mahasiswa: Mahasiswa) // menambah data
+
+    fun getAllMhs(): Flow<List<Mahasiswa>> // mendapatkan semua data dengan flow
+
+    fun getMhs(nim: String): Flow<Mahasiswa> // untuk mengambil data mahasiswa berdasarkan NIM
+
+    suspend fun deleteMhs(mahasiswa: Mahasiswa) // menggunakan fungsi deleteMahasiswa dari Mahasiswa Dao menghapus data mahasiswa
+
+    suspend fun updateMhs(mahasiswa: Mahasiswa) // menggunakan fungsi updateMahasiswa dari Mahasiswa Dao memperbarui data mahasiswa
 }
