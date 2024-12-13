7d0f20f257403003648402f4af91cfafe64eab91
ulfhiraa <studyiraa@gmail.com>
2024-12-13 21:47:55 +0700

    ADD Fun toDetailUiEvent digunakan untuk mengubah objek Mahasiswa ke MahasiswaEvent.

--- app/src/main/java/com/example/pam_meet10/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/pam_meet10/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.pam_meet10.ui.viewmodel
 
+import com.example.pam_meet10.data.entity.Mahasiswa
+
 class DetailMhsViewModel (
 
 ){
@@ -11,4 +13,23 @@ data class DetailUiState(
     val isLoading: Boolean = false,
     val isError: Boolean = false,
     val errorMessage: String = ""
-)
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
+// Data class untuk menampung data yang akan ditampilkan di UI
+// Memindahkan data dari entity ke UI
+fun Mahasiswa.toDetailUiEvent() : MahasiswaEvent {
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
