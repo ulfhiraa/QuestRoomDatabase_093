950ff9cc900d4f1ebf14e939360af222dea60e24
ulfhiraa <studyiraa@gmail.com>
2024-12-13 22:42:39 +0700

    UPDATE kode DetailMhsViewModel

--- app/src/main/java/com/example/pam_meet10/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/pam_meet10/ui/viewmodel/DetailMhsViewModel.kt
@@ -9,8 +9,11 @@ import com.example.pam_meet10.ui.navigation.DestinasiDetail
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.WhileSubscribed
-import kotlinx.coroutines.flow.internal.NopCollector.emit
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class DetailMhsViewModel (
