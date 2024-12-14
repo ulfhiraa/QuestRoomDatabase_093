f3ccc57cc8f4911813e413262c1a3f6a72cc2cda
ulfhiraa <studyiraa@gmail.com>
2024-12-14 16:02:11 +0700

    ADD fungsi ComponentDetailMhs untuk menampilkan detail informasi mhs di VIEW [DetailMhsView]

--- app/src/main/java/com/example/pam_meet10/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/pam_meet10/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,10 +1,42 @@
 package com.example.pam_meet10.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ComponentDetailMhs( // menampilkan detail informasi mhs
+    modifier: Modifier = Modifier,
+    judul: String, // menampilkan judul dari informasi
+    isinya: String, // menampilkan isi dari informasi
+){
+    Column (
+        modifier = modifier.fillMaxWidth(),
+
+        horizontalAlignment = Alignment.Start // menyelaraskan konten (judul & isi) ke sebelah kiri layar.
+    ){
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
 
 @Composable
 private fun DeleteConfirmationDialog( // konfirmasi penghapusan data
