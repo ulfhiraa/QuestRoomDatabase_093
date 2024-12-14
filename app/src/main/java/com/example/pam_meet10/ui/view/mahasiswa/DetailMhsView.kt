d5b43c8e339e0900d673192c92fec670ebf70d6d
ulfhiraa <studyiraa@gmail.com>
2024-12-14 16:29:29 +0700

    ADD fungsi BodyDetailMhs untuk menampilkan detail informasi mhs dengan button hapus dan konfirmasi di VIEW [DetailMhsView]

--- app/src/main/java/com/example/pam_meet10/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/pam_meet10/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,16 +1,24 @@
 package com.example.pam_meet10.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,6 +26,72 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pam_meet10.data.entity.Mahasiswa
+import com.example.pam_meet10.ui.viewmodel.DetailUiState
+import com.example.pam_meet10.ui.viewmodel.toMahasiswaEntity
+
+@Composable
+fun BodyDetailMhs( // untuk menampilkan tampilan detail mahasiswa
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState = DetailUiState(), // isLoading, isError, ada data atau tidak ada data
+    onDeleteClick: () -> Unit = { } // Fungsi callback yang akan dipanggil saat pengguna mengonfirmasi penghapusan data.
+){
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+    when {
+        detailUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator() // tampilkan loading
+            }
+        }
+
+        detailUiState.isUiEventNotEmpty -> {
+            Column (
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ){
+                ItemDetailMhs(
+                    mahasiswa = detailUiState.detailUiEvent.toMahasiswaEntity(),
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button(
+                    onClick = {
+                        deleteConfirmationRequired = true
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+
+                if (deleteConfirmationRequired) {
+                    DeleteConfirmationDialog(
+                        onDeleteConfirm = {
+                            deleteConfirmationRequired = false
+                            onDeleteClick()
+                        },
+                        onDeleteCancel = { deleteConfirmationRequired = false },
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+
+        detailUiState.isUiEventEmpty -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Data tidak ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
 
 @Composable
 fun ItemDetailMhs( // untuk menampilkan informasi detail mahasiswa dalam bentuk card
